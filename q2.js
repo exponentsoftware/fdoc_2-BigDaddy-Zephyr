41e0a2d11a61c147498a55b26d3cbc2c37130525
bigdaddy-zephyr <anujduggal61@gmail.com>
2021-03-04 19:59:35 +0530

    second question

---
+++ q2.js
@@ -0,0 +1,72 @@
+const users = [
+{
+	name:'Brook', 
+	scores:75,
+	skills:['HTM', 'CSS', 'JS'],
+	age:16
+},
+{
+	name:'Alex', 
+	scores:80,
+	skills:['HTM', 'CSS', 'JS'],
+	age:18
+}, 
+{
+	name:'David', 
+	scores:75,
+	skills:['HTM', 'CSS'],
+	age:22
+}, 
+{
+	name:'John', 
+	scores:85,
+	skills:['HTM'],
+	age:25
+},
+{
+	name:'Sara',
+	scores:95,
+	skills:['HTM', 'CSS', 'JS'],
+	age: 26
+},
+{
+	name:'Martha', 
+	scores:80,
+	skills:['HTM', 'CSS', 'JS'],
+	age:18
+},
+{
+	name:'Thomas',
+	scores:90,
+	skills:['HTM', 'CSS', 'JS'],
+	age:20
+}
+];
+
+console.log(users.filter(user=>user.scores>85));
+
+// var user1={	name:'Jake',
+// 	scores:21,
+// 	skills:['HTM', 'CSS', 'JS'],
+// 	age:26}
+
+// addUser(user1);
+
+function addUser(user){
+for(let i=0;i<users.length;i++){
+	if(users.name==user.name){
+		return false;
+	}
+
+}users.push(user);
+
+}
+
+
+function addUserSkill(username,skill){
+for(let i=0;i<users.length;i++){
+	if(users[i].name==username){
+		users[i].skills.push(skill);
+	}
+}
+}
